da9d6ebbffe67944653900ec7c458e2d412972bd
GitAlexandr <sashabushuev03@mail.ru>
2022-09-14 17:40:55 +0300

    steampars

--- DZ/vkapi/vkapi.py
+++ DZ/vkapi/vkapi.py
@@ -5,6 +5,7 @@ import numpy as np
 import requests
 
 dotenv.load_dotenv()
+
 Token = os.environ.get("CHPOKEN")
 Method = "friends.get"
 Id = os.environ.get("MYID")
@@ -12,17 +13,22 @@ arr = []
 url = ("https://api.vk.com/method/{METHOD}?user_id={ID}&fields=bdate&access_token={TOKEN}&v=5.131".format(
     METHOD=Method, TOKEN=Token, ID=Id))
 res = requests.get(url=url)
-
 friend = res.json()["response"]["items"]
 
-for i in friend:
-    try:
-        friend_birth = (i["bdate"])
-        if len(friend_birth) > 8:
-            year = int(friend_birth[-4::])
-            arr.append(year)
-            if len(arr) > 15:
-                sort_year = np.mean(arr)
-                print(2022 - int(sort_year))
-    except:
-        pass
+
+def zapolni(arr, friend):
+    for i in friend:
+        try:
+            friend_birth = (i["bdate"])
+            if len(friend_birth) > 8:
+                year = int(friend_birth[-4::])
+                arr.append(year)
+        except:
+            pass
+    return arr
+
+
+arr = zapolni(arr, friend)
+
+sort_year = np.mean(arr)
+print(2022 - int(sort_year))
